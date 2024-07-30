8973a30aa1d90520840c9afb9da320545fec74c5
Pita <pita.sherwood@protonmail.com>
2024-07-30 04:36:06 -0500

    Initial commit

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,48 @@
+import { Link } from "lucide-react"
+import MaxWidthWrapper from "./MaxWidthWrapper"
+import { buttonVariants } from "./ui/button"
+
+const Navbar = () => {
+    const user = undefined
+    const isAdmin = false
+    return (
+         <nav className="sticky z-[100] h-14 insex-x-0 top-0 w-full border-b border-gray-200 
+                       bg-white/75 backdrop-blur-lg transition-all">
+        <MaxWidthWrapper>
+            <div className="flex h-14 items-center justify-between border-b border-zinc-200">
+                <Link href="/" className="flex z-40 font-semibold">
+                    good<span className="text-green600">bytes</span>
+                </Link>
+                <div className="h-full flex items-center space-x-4">
+                    {user ? (
+                        <>
+                            <Link href="/api/auth/logout" className={buttonVariants({
+                                size: "sm",
+                                variant: "ghost",
+                            })}>
+                                Sign out
+                            </Link>
+                            { isAdmin ? <Link href="/api/auth/logout" className={buttonVariants({
+                                size: "sm",
+                                variant: "ghost",
+                            })}>
+                                Dashboard
+                            </Link> : null }
+                            <Link href="/api/configure/upload" className={buttonVariants({
+                                size: "sm",
+                                variant: "ghost",
+                            })}>
+                                Create meal
+                            </Link>
+                        </>
+                    ) : (
+                    <></>
+                    )}
+                </div>
+            </div>
+        </MaxWidthWrapper>
+    </nav>
+    )
+}
+
+export default Navbar
