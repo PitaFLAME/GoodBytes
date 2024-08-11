efb0110a115339165944eeef785836b25cd478a6
Pita <pita.sherwood@protonmail.com>
2024-08-11 03:48:46 -0500

    Fixed collapsible favorites tab

--- src/components/Favorites.tsx
+++ src/components/Favorites.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { ChevronDown } from "lucide-react";
+import { ChevronDown, ChevronRight } from "lucide-react";
 import { getFavorites } from "./data/CustomerData"
 import TileButton from "./TileButton"
 import React from "react";
@@ -21,22 +21,22 @@ const Favorites = ({ email }: {
                 <h1 className="text-stone-800 font-extrabold text-4xl ml-8">
                     Favorites
                 </h1>
-                <ChevronDown className="text-color-800 h-8 w-8 ml-auto mr-8" />
+                { isExpanded ?
+                    <ChevronDown className="text-color-800 h-8 w-8 ml-auto mr-8" /> :
+                    <ChevronRight className="text-color-800 h-8 w-8 ml-auto mr-8" /> }
                 <input type="checkbox" className="absolute h-10 w-10 right-28 opacity-0 cursor-pointer"
                     checked={isExpanded}
                     onChange={openExpandible}
                     
                     />
             </div>
-
-            { isExpanded ? 
-            <div className="grid xl:grid-cols-3 lg:grid-cols-2 md:grid-cols-2 sm:grid-cols-1 md:gap-2 gap-4 mx-4
-                ">
+ 
+            <div className={`grid xl:grid-cols-3 lg:grid-cols-2 md:grid-cols-2 sm:grid-cols-1 md:gap-2 gap-4 mx-4
+                ${isExpanded ? '' : 'hidden'}`}>
                 {favs.map((itemID, index) => (
                     <TileButton id={itemID} animDelay={delays[index]} key={index} />
                 ))}
-            </div> : null
-            }
+            </div>
         </div>
     )
 }
